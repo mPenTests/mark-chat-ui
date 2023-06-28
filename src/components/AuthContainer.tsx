6244c7b7a3c0a91f37f0b8ed6b14aefd113d6c10
mPenTests <marko.prifti4@gmail.com>
2023-06-28 08:34:57 +0200

    Configured Auth Container

--- src/components/AuthContainer.tsx
+++ src/components/AuthContainer.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Login  from './Login';
+import Register from './Register';
 
-function AuthContainer() {
+
+interface AuthContainerProps {
+  type: string;
+}
+
+function AuthContainer({type}: AuthContainerProps) {
+
   return (
-    <div>AuthContainer</div>
+    <div className={"w-full flex h-screen justify-center items-center"}>
+      <div className='w-fit h-fit rounded-md p-4 text-center' style={{backgroundColor: 'rgb( 34, 30, 29 )'}}>
+        {type === 'login' ? <Login /> : <Register />}
+      </div>
+    </div>
   )
 }
 
