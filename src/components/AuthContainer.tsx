bec59f6dd0d25dbe2fa9177b7cae06adeabbc416
mPenTests <marko.prifti4@gmail.com>
2023-07-02 22:38:54 +0200

    Login Prototype

--- src/components/AuthContainer.tsx
+++ src/components/AuthContainer.tsx
@@ -3,6 +3,7 @@ import Login from './Login';
 import Register from './Register';
 import { motion } from 'framer-motion';
 
+
 interface AuthContainerProps {
   type: string;
 }
@@ -12,45 +13,9 @@ function AuthContainer({ type }: AuthContainerProps) {
 
   return (
     <>
-      <motion.svg
-        initial={{ opacity: 0 }}
-        animate={startAnimation && { opacity: [0, 1] }}
-        transition={{ duration: 2 }}
-        id="visual"
-        className="inline absolute z-10 top-0 left-0"
-        viewBox="0 0 900 600"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        version="1.1"
-      >
-        <rect x="0" y="0" width="900" height="600" fill="#021321"></rect>
-        <defs>
-          <linearGradient id="grad1_0" x1="33.3%" y1="0%" x2="100%" y2="100%">
-            <stop offset="20%" stop-color="#021321" stop-opacity="1"></stop>
-            <stop offset="80%" stop-color="#021321" stop-opacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id="grad2_0" x1="0%" y1="0%" x2="66.7%" y2="100%">
-            <stop offset="20%" stop-color="#021321" stop-opacity="1"></stop>
-            <stop offset="80%" stop-color="#021321" stop-opacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <g transform="translate(900, 0)">
-          <path
-            d="M0 459.7C-75.5 440 -151.1 420.4 -222 384.5C-292.9 348.7 -359.3 296.6 -398.1 229.9C-437 163.1 -448.3 81.5 -459.7 0L0 0Z"
-            fill="#cc8b29"
-          ></path>
-        </g>
-        <g transform="translate(0, 600)">
-          <path
-            d="M0 -459.7C76.1 -441 152.1 -422.2 223 -386.2C293.9 -350.3 359.5 -297.1 398.1 -229.9C436.7 -162.6 448.2 -81.3 459.7 0L0 0Z"
-            fill="#cc8b29"
-          ></path>
-        </g>
-      </motion.svg>
       <motion.div
-        className={`w-full flex h-screen justify-center relative items-center bg-auth-background bg-no-repeat bg-center bg-cover`}
+        className={`w-full flex h-screen justify-center relative overflow-hidden items-center`}
+        style={{backgroundSize: "100vw 100vh"}}
       >
         <motion.div
           animate={{ scale: [0, 1], rotate: [0, 360] }}
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from './api_requests';
+import { motion } from 'framer-motion';
+import { Spinner } from 'react-bootstrap';
+
 
 function Login() {
+  const [username, setUsername] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
+  const navigate = useNavigate()
+
+
   return (
     <>
-      <Form>
+      <Form as={motion.form} animate={error ? {x: [0, -10, 10, -10, 0 ]} : {x: 0}} transition={{duration: 0.4}} onSubmit={(event: React.ChangeEvent<HTMLFormElement>): void => {
+        event.preventDefault()
+        setLoading(true)
+        setTimeout(() => {
+          login(username, password)
+          .then(response => {
+            setLoading(false)
+            localStorage.setItem("access", response.data.access)
+            localStorage.setItem("refresh", response.data.refresh)
+            navigate("/")
+          })
+          .catch((err) => {
+            setError(true)
+            setLoading(false)
+            console.log(err.response.data)
+          })
+        }, 1000)
+
+      }}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address or username</Form.Label>
-          <Form.Control type="email" placeholder="Who are you?" />
+          <Form.Control type={"username"} required onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {setUsername(event.target.value)} } placeholder="Who are you?" />
           <Form.Text className="text-white">
             Register <Link to="/register" onClick={() => localStorage.setItem("register", "true")}>here</Link>
           </Form.Text>
@@ -17,10 +46,13 @@ function Login() {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {setPassword(event.target.value)} } required />
+          <Form.Text className="text-white">
+            Forgot the damn thing? Reset it <Link to={"/forgot-password"} onClick={() => localStorage.setItem("forgot", "true")}>here</Link>
+          </Form.Text>
         </Form.Group>
-        <Button variant="primary" type="submit" className='mt-6 w-full'>
-          Login
+        <Button variant="primary" type="submit" onClick={() => setError(false)}  className='mt-6 w-full'>
+          {loading ? <Spinner as={"span"} style={{ height: "20px", width: "20px" }} animation="border" variant="light" /> : "Login"}
         </Button>
       </Form>
     </>   
